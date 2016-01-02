6999452e9f13da7472b9d56c07f6de9ad5b7985a
Thiago Brito de Lima <thitheguy@gmail.com>
2016-01-02 14:39:56 -0200

    View file standards headings

--- js/number_view.js
+++ js/number_view.js
@@ -1,3 +1,21 @@
+/*
+ * @number_view.js
+ *
+ * @description: View Class
+ *
+ * Math main class which has been built to provide
+ * a quicker and refined use of Math's Java Script library.
+ *
+ * Object Oriented class providing getters and setters for
+ * all the instances of objects access and parsing data via JSON.
+ */
+
+
+
+/**
+ * @author: thiagolimasp@live.com (Thiago Lima)
+ */
+
 var mathUser = new TestMath(),
     target = document.getElementsByClassName("ceil-result")[0];
 
