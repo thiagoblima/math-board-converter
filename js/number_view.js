dc5a41f015b893b791d7943cfa3204435684290b
Thiago Brito de Lima <thitheguy@gmail.com>
2016-01-02 16:55:26 -0200

    View structure

--- js/number_view.js
+++ js/number_view.js
@@ -6,7 +6,7 @@
  * Math View walksthrough the DOM API fetching the elements
  * to receive data from the model>controller instances.
  *
- * Object Oriented class providing DOM elements 
+ * Object Oriented class providing DOM elements
  * all the instances of objects access and parsing data via JSON.
  */
 
@@ -16,10 +16,20 @@
  * @author: thiagolimasp@live.com (Thiago Lima)
  */
 
-var mathUser = new TestMath(),
-    target = document.getElementsByClassName("ceil-result")[0];
+  var Model = [];
+      Model = {
 
-    mathUser.setCeil(55.9);
-    target.innerHTML = mathUser.getCeil();
+      math:{
+          user: new TestMath(),
+          instances:{
+            exponential: document.getElementsByClassName("exponential-result")[0]
 
-    console.log(mathUser.getCeil());
+          }
+       }
+    };
+
+
+    Model.math.user.setExponential(10);
+    Model.math.instances['exponential'].innerHTML = Model.math.user.getExponential();
+
+    console.log(Model.math.user.getExponential());
