d259ca81fc1a514ae81d7bab361bf1ff268300e2
Thiago Brito de Lima <thitheguy@gmail.com>
2016-01-09 23:54:48 -0200

    App.js standards headings

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+/*
+ *@app.js
+ *
+ *@description: Nodejs server, application start.
+ *
+ *Express, Consolidate and Swig are set on
+ *node_modules as dependencies
+ */
+
+
+/**
+ *@author: thiagolimasp@live.com (Thiago Lima)
+ */
+
+
 var express = require('express'),
     cons    = require("consolidate"),
     app     = express(),
     port    = process.env.PORT || 3412;
+
+/**
+ *@description: genenral set up for express
+ *
+ *Setting views and the static path for renderimg
+ *swig templates on the build.
+ */
 
  app.engine('html', cons.swig);
  app.set('view engine', 'html');
@@ -9,5 +31,5 @@ var express = require('express'),
  app.use(express.static( __dirname + '/public'));
  app.use(app.router);
 
-app.listen(port);
+ app.listen(port);
  console.log("Math Server Started");
