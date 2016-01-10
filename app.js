3ef2bb3be3fe789a88f3a611eeda9610f31e5e64
Thiago Brito de Lima <thitheguy@gmail.com>
2016-01-09 22:28:15 -0200

    Retuned the title on top

--- app.js
+++ app.js
@@ -9,22 +9,5 @@ var express = require('express'),
  app.use(express.static( __dirname + '/public'));
  app.use(app.router);
 
-
- function errorHandler(err, req, res, next){
-     console.error(err.message);
-     console.error(err.stack);
-     res.status(500);
-     res.render('error_template', { error: err });
-
- };
-
- app.get('/:index', function(req, res, next){
-   var index = req.params.index;
-   res.render("index", {index : index});
- });
-
-
-app.use(errorHandler);
-
 app.listen(port);
  console.log("Math Server Started");
