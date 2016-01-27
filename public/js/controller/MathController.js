f83389fb80bfdfe53e86753acff2c9958b684b42
Thiago Brito de Lima <thitheguy@gmail.com>
2016-01-27 21:31:16 -0200

    Math Controller receives as html data

--- public/js/controller/MathController.js
+++ public/js/controller/MathController.js
@@ -5,4 +5,11 @@ app.controller("MathController", [ '$scope', function($scope){
     subtitle: "Converting all Math's Task"
   };
 
+  $scope.threeCollumn = {
+    paragraphs:{
+      collumnOne: 'Here\'s your quick and smooth math converter, anytime <span>you</span> want!',
+      collumnTwo: 'Get registered and receive the latest math news, anytime <span>you</span> want!',
+      collumnThree: 'Share get a lot of discounts on our latest services made for <span>you</span>!'
+    }
+  };
 }]);
