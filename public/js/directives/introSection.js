cb06d6105eaacc8293e0d1c78ce3117d6b6495c0
Thiago Lima <thitheguy@gmail.com>
2016-02-08 21:14:10 -0200

    Head standards class

--- public/js/directives/introSection.js
+++ public/js/directives/introSection.js
@@ -1,3 +1,10 @@
+/**
+ *@name: introSection.js
+ *@description: Intro section is now divided and being rendered as template
+ *HTML5 / AngularJS directive which has data parsing data through the scope
+ */
+
+
 app.directive('introSection', function(){
   return{
     restrict: 'E',
