0aea77a841d232d23c0117064cbb74b1aabd3d1c
Thiago Lima <thitheguy@gmail.com>
2016-02-08 21:13:30 -0200

    Head standards class

--- public/js/directives/headSection.js
+++ public/js/directives/headSection.js
@@ -1,3 +1,10 @@
+/**
+ *@name: headSection.js
+ *@description: AngularJS directive set to hold the header as template
+ *being parsed data through the scope as data.
+ */
+
+
 app.directive('headSection', function(){
   return{
      restrict: 'E',
