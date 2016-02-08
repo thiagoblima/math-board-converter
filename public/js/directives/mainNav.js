1f5f5c3f8b5c27fc5c4475241a5849880f7c1e5c
Thiago Lima <thitheguy@gmail.com>
2016-02-08 21:14:57 -0200

    Head Standards class

--- public/js/directives/mainNav.js
+++ public/js/directives/mainNav.js
@@ -1,3 +1,9 @@
+/**
+ *@name: mainNav.js
+ *@description: AngularJS directive with the purpose of setting a better
+ *HTML5 organization, having a proper HTML5 template parsing data
+ */
+
 app.directive('mainNav', function(){
    return{
      restrict: 'E',
