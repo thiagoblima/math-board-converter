0d52cbfe8e6b6cf202cd306087979c8dc9e4040a
Thiago Lima <thitheguy@gmail.com>
2016-03-26 17:55:27 -0300

    Adding the app.js the new path to build

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var express = require('express'),
  app.engine('html', cons.swig);
  app.set('view engine', 'html');
  app.set("views", __dirname + '/public');
- app.use(express.static( __dirname + '/public'));
+ app.use(express.static( __dirname + '/public/build'));
 
  app.listen(port);
  console.log("Math Server Started");
