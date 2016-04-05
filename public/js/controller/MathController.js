a12c4b9598c5b47512d8e60dd868caeda1815dcb
Thiago Lima <thitheguy@gmail.com>
2016-04-04 23:20:57 -0300

    Controller organization

--- public/js/controller/MathController.js
+++ public/js/controller/MathController.js
@@ -19,10 +19,12 @@ app.controller("MathController", [ '$scope', function($scope){
   };
 
   $scope.table = {
+
     intro:{
-      text: "Math Quick Tasks"  
+      text: "Math Quick Tasks"
     },
 
     title: [ 'Esponential', 'Fixed', 'Precision', 'String', 'Ceil', 'Floor', 'Round', 'Absolute' ]
   };
+
 }]);
