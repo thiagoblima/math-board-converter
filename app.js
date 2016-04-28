be9913b972a31dc11fff340d3b4c1012ed7251a4
Thiago Lima <thitheguy@gmail.com>
2016-04-27 22:05:42 -0300

    Another server now set up

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var express = require('express'),
  *swig templates on the build.
  */
 
- app.use(express.static( __dirname + '/public'));
+ app.use(express.static( __dirname + '/public/build'));
  app.listen(port);
 
  connect.sucess();
