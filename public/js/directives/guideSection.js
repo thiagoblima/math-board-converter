0d99b3d981b56e2fa09f566cc6f1253b9ad4f746
Thiago Lima <thitheguy@gmail.com>
2016-05-03 23:49:11 -0300

    Guide Section A 05

--- public/js/directives/guideSection.js
+++ public/js/directives/guideSection.js
@@ -3,3 +3,13 @@
  *@description: AngularJS directive set to hold the guide-section as template
  *being parsed data through the scope as data.
  */
+
+app.directive("guideSection", function(){
+   return{
+     restrict: "E",
+     scope:{
+      data: "=" 
+     },
+     templateUrl: 'js/directives/views/guideSection.html'
+   };
+});
