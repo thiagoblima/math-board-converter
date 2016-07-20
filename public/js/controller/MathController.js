46cc039eff4d72fc8364fa2d109fd1fbcb12d47a
Thiago Lima <thitheguy@gmail.com>
2016-07-19 22:02:00 -0300

    Intro Section Component A005

--- public/js/controller/MathController.js
+++ public/js/controller/MathController.js
@@ -78,11 +78,22 @@ GuideController.$inject = ['$scope'];
        title: 'Welcome, here you\'re going to find Math conversion easily'
      },
 
-     paragraphs:{
-       collumnOne: 'Here\'s your quick and smooth math converter, anytime <span>you</span> want!',
-       collumnTwo: 'Get registered and receive the latest math news, anytime <span>you</span> want!',
-       collumnThree: 'Share get a lot of discounts on our latest services made for <span>you</span>!'
-     }
+     paragraphs:[
+      
+      { 
+        description: 'Here\'s your quick and smooth math converter, anytime <span>you</span> want!',
+        class: 'col-md-4'
+      },
+      { 
+        description: 'Get registered and receive the latest math news, anytime <span>you</span> want!',
+        class: 'col-md-4' 
+      },
+      { 
+        description: 'Share get a lot of discounts on our latest services made for <span>you</span>!',
+        class: 'col-md-4' 
+      }
+
+     ]
 
    };
  }
