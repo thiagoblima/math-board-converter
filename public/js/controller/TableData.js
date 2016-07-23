ec092a0c9d7e53747d15ec81d29f71093d6f1a5b
Thiago Lima <thitheguy@gmail.com>
2016-07-23 01:44:27 -0300

    Table Controller A028

---
+++ public/js/controller/TableData.js
@@ -0,0 +1,39 @@
+/**
+  *@author: Thiago Lima
+  *
+  *@description: Table controller, responsable for scope object table data,
+  *settings and enchancement model view architecture.
+  *
+  */
+
+
+app.controller('TableController', TableController);
+
+
+//TableController.$inject = ['$scope'];
+
+
+function TableController(){
+
+   var vm = this;   
+   vm.table = {
+
+     intro:{
+       text: "Math Quick Tasks"
+     },
+
+     title: [
+        {name: 'Esponential'},
+        {name: 'Fixed'},
+        {name: 'Precision'},
+        {name: 'String'},
+        {name: 'Ceil'},
+        {name: 'Floor'},
+        {name: 'Round'},
+        {name: 'Absolute'}
+     ]
+   };
+
+   vm.info = { test: "testing" };
+
+ }
