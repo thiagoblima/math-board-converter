b6f5ce1c857b6283342e919ccf50833168293108
Thiago Lima <thitheguy@gmail.com>
2016-07-24 15:28:44 -0300

    Math Filter A012

--- public/js/filters/mathFilter.js
+++ public/js/filters/mathFilter.js
@@ -3,3 +3,13 @@
  *@description: AngularJS filter created to perform Math core
  * library to convert the table values.
  */
+
+app.filter('ceilFilter', function(){
+    return function(input){
+      if(input){
+        if (input === undefined) input = "";
+        if (isNaN(input)) input = "Insert a number";
+        return Math.ceil(input);
+      } 
+    };
+});
