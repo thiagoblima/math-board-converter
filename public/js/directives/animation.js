6d84bf49aa94d01fe9e8f6680899b23ce24a2364
Thiago Lima <thitheguy@gmail.com>
2016-07-24 21:59:59 -0300

    Angular Animate A016

---
+++ public/js/directives/animation.js
@@ -0,0 +1,12 @@
+/**
+ *@name: animate.js
+ *@description: Main file to write the Java Script AngularJS animations.
+ *Basicaly animations won't require this file because most of them are
+ *based on ng-show/hide tags, something special may be written here.
+ */
+
+app.directive('animateTest', function($animate, $tmeout){
+   return function(scope, elem, attr){
+
+  };  
+});
