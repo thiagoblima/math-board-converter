f02fbd9854059650ee5b784528b8671abd9008c7
Thiago Lima <thitheguy@gmail.com>
2016-08-04 23:10:23 -0300

    Table Angularjs Component A010

---
+++ public/js/directives/tableSection.js
@@ -0,0 +1,14 @@
+/**
+ *@name: headSection.js
+ *@description: AngularJS directive set to hold the table-section as template
+ *being parsed data through the scope as data.
+ */
+
+app.directive("tableSection", function(){
+   return{
+     restrict: "EA",
+     templateUrl: 'js/directives/views/tableSection.html',
+     controllerAs:'mv',
+     bindToController: true
+   };
+});
