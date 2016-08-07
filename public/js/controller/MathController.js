fe7fe242824b056a6a2087b66186240f3b3d9ef1
Thiago Lima <thitheguy@gmail.com>
2016-08-07 16:58:29 -0300

    Intro Controller Refactor A007

--- public/js/controller/MathController.js
+++ public/js/controller/MathController.js
@@ -27,7 +27,7 @@ app.controller('NavController', NavController);
 app.controller('IntroController', IntroController);
 app.controller('TableController', TableController);
 app.controller('GuideController', GuideController);
-  
+
 
 
 
@@ -50,135 +50,139 @@ GuideController.$inject = ['$scope'];
 
 
 
- function HeadController($scope){
-   $scope.header = [
-      { 
-        settings: {
-          title: "Math Board Converter", 
-          subtitle: "Converting all Math's Task" 
-        } 
+function HeadController($scope) {
+  $scope.header = [
+    {
+      settings: {
+        title: "Math Board Converter",
+        subtitle: "Converting all Math's Task"
       }
-   ];
- }
+    }
+  ];
+}
 
 
- function NavController($scope){
-   $scope.nav = {
-       settings:[
-         {
+function NavController($scope) {
+  $scope.nav = {
+    settings: [
+      {
 
-            name: 'home',
-            color:'color-1',
-            front:'front',
-            back:'back',
-            icon:'fa fa-home fa-4x'
+        name: 'home',
+        color: 'color-1',
+        front: 'front',
+        back: 'back',
+        icon: 'fa fa-home fa-4x'
 
-         },
-         {
-           
-            name: 'blog',
-            color:'color-2',
-            front:'front',
-            back:'back',
-            icon:'fa fa-align-left fa-4x'
+      },
+      {
 
-         },
-         {
-           
-            name: 'photos',
-            color:'color-3',
-            front:'front',
-            back:'back',
-            icon:'fa fa-camera-retro fa-4x'
+        name: 'blog',
+        color: 'color-2',
+        front: 'front',
+        back: 'back',
+        icon: 'fa fa-align-left fa-4x'
 
-         },
-         {
-           
-            name: 'about',
-            color:'color-4',
-            front:'front',
-            back:'back',
-            icon:'fa fa-user fa-4x'
+      },
+      {
 
-          },
-          {
-           
-            name: 'contact',
-            color:'color-5',
-            front:'front',
-            back:'back',
-            icon:'fa fa-comments fa-4x'
+        name: 'photos',
+        color: 'color-3',
+        front: 'front',
+        back: 'back',
+        icon: 'fa fa-camera-retro fa-4x'
 
-          }
-       ]
-   };
- }
+      },
+      {
+
+        name: 'about',
+        color: 'color-4',
+        front: 'front',
+        back: 'back',
+        icon: 'fa fa-user fa-4x'
+
+      },
+      {
+
+        name: 'contact',
+        color: 'color-5',
+        front: 'front',
+        back: 'back',
+        icon: 'fa fa-comments fa-4x'
+
+      }
+    ]
+  };
+}
 
 
- function IntroController($scope){
-   $scope.intro = {
+function IntroController() {
 
-     head:{
+  var vm = this;
+
+  vm.intro = {
+
+    head: {
       title: 'Math Board Coverter'
-     },
+    },
 
-     header:{
-       title: 'Welcome, here you\'re going to find Math conversion easily'
-     },
+    header: {
+      title: 'Welcome, here you\'re going to find Math conversion easily'
+    },
 
-     paragraphs:[
-      
-      { 
+    paragraphs: [
+
+      {
         description: 'Here\'s your quick and smooth math converter, anytime <span>you</span> want!',
         class: 'col-md-4'
       },
-      { 
+      {
         description: 'Get registered and receive the latest math news, anytime <span>you</span> want!',
-        class: 'col-md-4' 
+        class: 'col-md-4'
       },
-      { 
+      {
         description: 'Share get a lot of discounts on our latest services made for <span>you</span>!',
-        class: 'col-md-4' 
+        class: 'col-md-4'
       }
 
-     ]
+    ]
 
-   };
- }
+  };
+
+}
 
 
 
- function GuideController(){
+function GuideController() {
 
-   var vm = this;
+  var vm = this;
 
-   vm.guide = {
+  vm.guide = {
 
-        header:{
-           description: 'Guide: Follow the walkthrough and share:'
-        },
+    header: {
+      description: 'Guide: Follow the walkthrough and share:'
+    },
 
-        text:[
-            {
-              name: "study", 
-              class:'col-md-4',
-              glyphicon:"glyphicon-education", 
-              description:"Study cases for your career. Get Certified in a year."
-            },
-            {
-              name: "development", 
-              class:'col-md-4',
-              glyphicon:"glyphicon-stats", 
-              description:"Constant development and great results in a month."
-            },
-            {
-              name: "business", 
-              class:'col-md-4',
-              glyphicon: "glyphicon-briefcase", 
-              description:"The best for your business and newtwork development."
-            },
-        ]
+    text: [
+      {
+        name: "study",
+        class: 'col-md-4',
+        glyphicon: "glyphicon-education",
+        description: "Study cases for your career. Get Certified in a year."
+      },
+      {
+        name: "development",
+        class: 'col-md-4',
+        glyphicon: "glyphicon-stats",
+        description: "Constant development and great results in a month."
+      },
+      {
+        name: "business",
+        class: 'col-md-4',
+        glyphicon: "glyphicon-briefcase",
+        description: "The best for your business and newtwork development."
+      },
+    ]
 
-   };
+  };
 
- }
+}
