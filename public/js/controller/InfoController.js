2be69fb3fe14cb25a3e08deb8eefbd10119e5e76
Thiago Lima <thitheguy@gmail.com>
2016-08-08 21:22:59 -0300

    InfoController Head A007

---
+++ public/js/controller/InfoController.js
@@ -0,0 +1,51 @@
+/**
+  *@author: Thiago Lima
+  *
+  *@description: General Controllers class, they're separated
+  *into sections to get easier and more readable.
+  *
+  *@name: InfoController storing information for head section.
+  *
+  */
+
+
+
+  /**
+  *@name: app
+  *
+  *@description: Controllers import, all of them should
+  *be imported underneath.
+  *
+  */
+
+
+app.controller('HeadInfoController', HeadInfoController);
+
+
+
+/**
+  *@name: Dependency Injection
+  *
+  *@description: This is where you just import the
+  *dependencies to the $scope, never forget it, otherwise
+  *app won't work correctly and may cause a break on the components.
+  *
+  */
+
+
+HeadInfoController.$inject = ['$scope'];
+
+
+function HeadInfoController(){
+
+  var vm = this;
+
+  vm.title = {
+     name: 'Math Board Converter'
+  };
+
+  vm.meta = {
+    
+  };
+
+}
