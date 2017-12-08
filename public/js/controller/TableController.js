7efaf212c27dd7ba74ab522b5b3e2637f4e1d3bc
Thiago Lima <thitheguy@gmail.com>
2017-12-08 21:50:27 -0200

    Code reviewing Angular 1.x

--- public/js/controller/TableController.js
+++ public/js/controller/TableController.js
@@ -1,88 +1,67 @@
 /**
-  *@author: Thiago Lima
-  *
-  *@description: Table controller, responsable for scope object table data,
-  *settings and enchancement model view architecture.
-  *
-  */
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @name Table Controller
+ * @description: Responsable for scope object table data,
+ * settings and enchancement model view architecture.
+ */
 
-
-
-app.controller('TableController', TableController);
-app.controller('MessagesController', MessagesController);
-
-
-
+app.controller("TableController", TableController);
+app.controller("MessagesController", MessagesController);
 
 /**
-  *@name: Dependency Injection
-  *
-  *@description: This is where you just import the
-  *dependencies to the $scope, never forget it, otherwise
-  *app won't work correctly and may cause a break on the components.
-  *
-  */
+ * @name: Dependency Injection
+ * @description: This is where you just import the
+ * dependencies to the $scope, never forget it, otherwise
+ * app won't work correctly and may cause a break on the components.
+ */
 
+TableController.$inject = ["$scope"];
+MessagesController.$inject = ["$scope"];
 
+function TableController() {
+  var vm = this;
 
-TableController.$inject = ['$scope'];
-MessagesController.$inject = ['$scope'];
+  vm.textFun = textFun();
+  vm.clearExponential = clearExponential();
+  vm.table = table;
 
+  function textFun() {
+    vm.val = "-";
+    return vm.val.replace(/-/i, "Java Script type methods");
+  }
 
+  function clearExponential() {
+    vm.value = "-";
+    return vm.value.replace(/-/i, "Have Fun!");
+  }
 
-function TableController(){
+  vm.table = {
+    intro: {
+      text: "Number Testing Tool"
+    },
 
-   var vm = this;   
-
-   vm.textFun = textFun();
-   vm.clearExponential = clearExponential();
-   vm.table = table;
-
-   function textFun() {
-     vm.val = '-'; 
-     return vm.val.replace(/-/i, "Java Script type methods");
-   }
-
-   function clearExponential() {
-     vm.value = '-';
-     return vm.value.replace(/-/i, "Have Fun!");
-   }
-   
-   vm.table = {
-
-     intro:{
-       text: "Number Testing Tool"
-     },
-
-     title: [
-        {name: 'Esponential'},
-        {name: 'Fixed'},
-        {name: 'Precision'},
-        {name: 'String'},
-        {name: 'Ceil'},
-        {name: 'Floor'},
-        {name: 'Round'},
-        {name: 'Absolute'}
-     ],
-  
+    title: [
+      { name: "Esponential" },
+      { name: "Fixed" },
+      { name: "Precision" },
+      { name: "String" },
+      { name: "Ceil" },
+      { name: "Floor" },
+      { name: "Round" },
+      { name: "Absolute" }
+    ]
   };
- 
 }
 
-function MessagesController(){
-
+function MessagesController() {
   var vm = this;
 
   vm.messages = {
-    
-    settings:[
-
-      { name:'required', message:'Enter a number' },
-      { name:'pattern', message:'Wrong pattern' },
-      { name:'minlength', message:'Too short!' },
-      { name:'maxlength', message:'Too long!' }
-
+    settings: [
+      { name: "required", message: "Enter a number" },
+      { name: "pattern", message: "Wrong pattern" },
+      { name: "minlength", message: "Too short!" },
+      { name: "maxlength", message: "Too long!" }
     ]
-
   };
 }
