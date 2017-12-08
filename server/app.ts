93f435b5149fa72a9f1012887e444b41a5ecb205
Thiago Lima <thitheguy@gmail.com>
2017-12-08 01:19:37 -0200

    Creating a NodeJS Server (Type Script)

---
+++ server/app.ts
@@ -0,0 +1,31 @@
+/**
+ * @name: app.ts
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @description: Main entrance of the entire server 
+ * application.
+ * @see: All dependencies and 
+ * 
+ */
+
+import * as express from "express";
+import { Server } from './server';
+
+
+/**
+ * @see: store port value in a
+ * variable in to follow the 
+ * Server @constructor design.
+ * @param: app { express() }
+ * @param: port { Number } 
+ */
+
+
+let port = 3412; 
+let server = new Server(express(), port);
+
+/**
+ * @method: { run } 
+ */
+
+server.run();
+console.info(`listening on ${port}`);
