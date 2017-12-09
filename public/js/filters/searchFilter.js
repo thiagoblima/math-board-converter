6f080cd0c492221b39ea3e484e7b8c095be64b53
Thiago Lima <thitheguy@gmail.com>
2017-12-09 00:26:10 -0200

    Code reviewing Angular 1.x

--- public/js/filters/searchFilter.js
+++ public/js/filters/searchFilter.js
@@ -1,16 +1,16 @@
 /**
- *@name: searchFilter.js
- *@description: AngularJS filter created to perform on search bar
- * that's inside header.
+ * @name: searchFilter.js
+ * @description: filter created to perform on search bar
+ * that's into header-section.
  */
 
-app.filter('searchFilter', function(){
-  return function(text, length, end){
-    if (text){
-       if(isNaN(length)) length = 0;
-       if(end === undefined) end = "...";
-       if(text.length <= length) return text;
-       else return String(text).substring(0, length) + end;
+app.filter("searchFilter", function() {
+  return function(text, length, end) {
+    if (text) {
+      if (isNaN(length)) length = 0;
+      if (end === undefined) end = "...";
+      if (text.length <= length) return text;
+      else return String(text).substring(0, length) + end;
     }
   };
 });
