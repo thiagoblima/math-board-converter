919f1fbb8903b803ac36d316b05f4d31a37274d0
Thiago Lima <thitheguy@gmail.com>
2017-12-09 00:26:37 -0200

    Code reviewing Angular 1.x

--- public/js/filters/mathFilter.js
+++ public/js/filters/mathFilter.js
@@ -1,119 +1,117 @@
 /**
- *@name: mathFilter.js
- *@description: AngularJS filter created to perform Math core
- * library to convert the table values.
+ * @name: mathFilter.js
+ * @description: filters created to perform Math core
+ * library to convert table values.
  */
 
-
-/*
- *@name: exponentialFilter 
+/**
+ * @name: exponentialFilter
  */
 
-app.filter('exponentialFilter', function(){
-   return function(input){
-     if(parseInt(input)){
-       if(input === undefined) input = "";
-       if(isNaN(input)) input = "Insert any number";
-       return JSON.stringify([ { result: input.toExponential() } ]);  
-     }
-   };
+app.filter("exponentialFilter", function() {
+  return function(input) {
+    if (parseInt(input)) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert any number";
+      return JSON.stringify([{ result: input.toExponential() }]);
+    }
+  };
 });
 
-/*
- *@name: fixedFilter 
+/**
+ * @name: fixedFilter
  */
 
-app.filter('fixedFilter', function(){
-   return function(input){
-     if(input){
-       if(input === undefined) input = "";
-       if(isNaN(input)) input = "Insert any number";
-       return JSON.stringify([ { result: input.toFixed(2) } ]);  
-     }  
-   };
+app.filter("fixedFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert any number";
+      return JSON.stringify([{ result: input.toFixed(2) }]);
+    }
+  };
 });
 
-/*
- *@name: precisionFilter 
+/**
+ * @name: precisionFilter
  */
 
-app.filter('precisionFilter', function(){
-   return function(input){
-     if(input){
-       if(input === undefined) input = "";
-       if(isNaN(input)) input = "Insert any number";
-       return JSON.stringify([ { result: input.toPrecision() } ]);  
-     }  
-   };
+app.filter("precisionFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert any number";
+      return JSON.stringify([{ result: input.toPrecision() }]);
+    }
+  };
 });
 
-/*
- *@name: stringFilter 
+/**
+ * @name: stringFilter
  */
 
-app.filter('stringFilter', function(){
-   return function(input){
-     if(input){
-       if(input === undefined) input = "";
-       if(isNaN(input)) input = "Insert any number";
-       return JSON.stringify([ { result: input.toString() } ]);  
-     }
-   };
+app.filter("stringFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert any number";
+      return JSON.stringify([{ result: input.toString() }]);
+    }
+  };
 });
 
-
-/*
- *@name: ceilFilter 
+/**
+ * @name: ceilFilter
  */
 
-app.filter('ceilFilter', function(){
-    return function(input){
-      if(input){
-        if (input === undefined) input = "";
-        if (isNaN(input)) input = "Insert a float number";
-        return JSON.stringify([ { result: Math.ceil(input) } ]);
-      } 
-    };
+app.filter("ceilFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert a float number";
+      return JSON.stringify([{ result: Math.ceil(input) }]);
+    }
+  };
 });
 
-/*
- *@name: floorFilter 
+/**
+ * @name: floorFilter
  */
 
-app.filter('floorFilter', function(){
-    return function(input){
-      if(input){
-        if(input === undefined) input = "";
-        if(isNaN(input)) input = "Insert a float number";
-        return JSON.stringify([ { result: Math.floor(input) } ]);  
-      }
-    };
+app.filter("floorFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert a float number";
+      return JSON.stringify([{ result: Math.floor(input) }]);
+    }
+  };
 });
 
-/*
- *@name: roundFilter 
+/**
+ * @name: roundFilter
  */
 
-app.filter('roundFilter', function(){
-    return function(input){
-      if(input){
-        if(input === undefined) input = "";
-        if(isNaN(input)) input = "Insert a float number";
-        return JSON.stringify([ { result: Math.round(input) } ]); 
-      }
-    };
+app.filter("roundFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "Insert a float number";
+      return JSON.stringify([{ result: Math.round(input) }]);
+    }
+  };
 });
 
-/*
- *@name: absoluteFilter 
+/**
+ * @name: absoluteFilter
  */
 
-app.filter('absoluteFilter', function(){
-   return function(input){
-     if(input){
-       if(input === undefined) input = "";
-       if(isNaN(input)) input = "This must be a number";
-       return JSON.stringify([ { result:Math.abs(input) } ]);  
-     }
-   };
+app.filter("absoluteFilter", function() {
+  return function(input) {
+    if (input) {
+      if (input === undefined) input = "";
+      if (isNaN(input)) input = "This must be a number";
+      return JSON.stringify([{ result: Math.abs(input) }]);
+    }
+  };
 });
