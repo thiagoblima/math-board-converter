bb63c9f61721fcf465ebad0b1012fe826e3b0965
Thiago Lima <thitheguy@gmail.com>
2017-12-08 22:00:29 -0200

    Code reviewing Angular 1.x

--- public/js/directives/animation.js
+++ public/js/directives/animation.js
@@ -1,8 +1,8 @@
 /**
- *@name: animate.js
- *@description: Main file to write the Java Script AngularJS animations.
- *Basicaly animations won't require this file because most of them are
- *based on ng-show/hide tags, something special may be written here.
+ * @name: animate.js
+ * @description: Main file to write the Java Script AngularJS animations.
+ * Basicaly animations won't require this file because most of them are
+ * based on ng-show/hide tags, something special may be written here.
  */
 
 app.directive('animateTest', function($animate){
