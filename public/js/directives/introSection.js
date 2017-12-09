38faa98c3302dfe97e85e863b5eff2ea9f804cdc
Thiago Lima <thitheguy@gmail.com>
2017-12-08 22:33:48 -0200

    Code reviewing Angular 1.x

--- public/js/directives/introSection.js
+++ public/js/directives/introSection.js
@@ -1,16 +1,14 @@
 /**
- *@name: introSection.js
- *@description: Intro section is now divided and being rendered as template
- *HTML5 / AngularJS directive which has data parsing data through the scope
+ * @name: introSection.js
+ * @description: intro set component intro-section
+ * Binding data to its scope object.
  */
 
-
-
-app.directive('introSection', function(){
-  return{
-    restrict: 'EA',
-    templateUrl:'js/directives/views/introSection.html',
-    controllerAs:"vm",
+app.directive("introSection", function() {
+  return {
+    restrict: "EA",
+    templateUrl: "js/directives/views/introSection.html",
+    controllerAs: "vm",
     bindToController: true
   };
 });
