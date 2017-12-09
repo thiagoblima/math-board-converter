1bdd7f3c943106f0e9673916d732b717b2719232
Thiago Lima <thitheguy@gmail.com>
2017-12-09 05:26:19 -0200

    Logging Middleware

---
+++ server/middleware/requestLogger.ts
@@ -0,0 +1,19 @@
+/**
+ * @name: requestLogger
+ * @author: <thiagolimasp@live.com> Thiago Lima
+ * @description: Middleware responsible for logging
+ * REST API requests.
+ */
+
+import * as express from "express";
+
+const requestLogger: express.RequestHandler = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+) => {
+    console.info(`${(new Date()).toUTCString()}|${request.method}|${request.url}|${request.ip}`);
+    next();
+}
+
+export default requestLogger;
