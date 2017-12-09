cd92b345c3c33bedd269ed1859986241ecddd04d
Thiago Lima <thitheguy@gmail.com>
2017-12-09 21:49:01 -0200

    GET USER REST API

--- server/routes/userRouter.ts
+++ server/routes/userRouter.ts
@@ -1,16 +1,18 @@
 /**
- * @name: userRouter 
+ * @name: userRouter
  * @author: <thiagolimasp@live.com> Thiago Lima
  * @description: User REST API, all the user endpoints
  * should be here, then called on main server.
  */
 
 import * as express from "express";
+import User from "../models/userModel";
 
 const userRouter = express.Router();
 
-userRouter.get("/user", (request: express.Request, response: express.Response) => {
-    
+userRouter.get(
+  "/user",
+  (request: express.Request, response: express.Response) => {
     let seedData = {
       id: 1,
       firstName: "thiago",
@@ -18,6 +20,25 @@ userRouter.get("/user", (request: express.Request, response: express.Response) =
     };
 
     response.send(seedData);
+  }
+);
+
+userRouter.get(
+  "/users",
+  (request: express.Request, response: express.Response) => {
+    // A simple GET request method to grab user in the database
+    User.find((err, user) => {
+      if (err) throw err;
+
+      if (!user) {
+        return response.status(401).send({
+          success: false,
+          msg: "Authentication failed. User not found."
+        });
+      } else {
+        response.status(200).json(user);
+      }
+    });
   }
 );
 
