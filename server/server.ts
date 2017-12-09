59da9eea1d8e67f14a6b676a9439ce14bd0e275e
Thiago Lima <thitheguy@gmail.com>
2017-12-09 05:30:20 -0200

    Adding the middelware and user REST API route to the application

--- server/server.ts
+++ server/server.ts
@@ -1,26 +1,31 @@
 /**
  * @class: Server
  * @author: <thiagolimasp@live.com> Thiago Lima
- * @description: The main nodejs server is going to be 
- * organized into controllers. 
+ * @description: The main nodejs server is going to be
+ * organized into controllers.
  * @see: More detailed info can be found on README.md
  * as well as on controllers' folder.
  */
 
 import * as express from "express";
-
+import userRouter from "./routes/userRouter";
+import requestLogger from "./middleware/requestLogger";
 export class Server {
   /**
    * @param app - { express application }
    * @param port - { port to listen on }
    */
   constructor(private app: express.Express, private port: number) {
-    //add here the controllers
-    /**
-     * @example:
-     * this.configureMiddleware(app);
-     * this.configureRoutes(app);
-     */
+    this.configureMiddleware(app);
+    this.configureRoutes(app);
+  }
+
+  private configureMiddleware(app: express.Express) {
+    app.use(requestLogger);
+  }
+
+  private configureRoutes(app: express.Express) {
+    app.get("/user", userRouter);
   }
 
   /**
