29273e1e33372a2138342e63870a30d468695bbd
Thiago Lima <thitheguy@gmail.com>
2017-12-13 02:05:38 -0200

    Creating a passport class

---
+++ server/config/passport.ts
@@ -0,0 +1,43 @@
+/**
+ * @class: Passport
+ * @author: Thiago Lima <thiagolimasp@live.com>
+ * @description: The passport-jwt strategy file.
+ * @see: user model and database file are imported here.
+ * @module: App { server }
+ */
+
+import User from "../models/userModel";
+import { MongoDB } from "../config/database";
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongodb = new MongoDB();
+
+export class Passport {
+  private opts: {};
+  constructor() {}
+
+  public setJWTRequest(): Object {
+    return (this.opts = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+      secretOrKey: mongodb.getSecret()
+    });
+  }
+
+  public run(passport): void {
+    passport.use(
+      new JwtStrategy(this.setJWTRequest(), (jwt_payload, done) => {
+        User.findOne({ id: jwt_payload.id }, (err, user) => {
+          if (err) {
+            return done(err, false);
+          }
+          if (user) {
+            done(null, user);
+          } else {
+            done(null, false);
+          }
+        });
+      })
+    );
+  }
+}
