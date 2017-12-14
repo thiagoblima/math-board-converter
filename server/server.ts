87c5b22422b9a85ccc985fd1ff142bf7316cc359
Thiago Lima <thitheguy@gmail.com>
2017-12-14 16:40:33 -0200

    Adding /api/ as prefix on the method app.Use()

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,7 @@
 /**
- * @class: Server
- * @author: <thiagolimasp@live.com> Thiago Lima
- * @description: The main nodejs server is going to be
- * organized into controllers.
- * @see: More detailed info can be found on README.md
- * as well as on controllers' folder.
+ * @class      : Server
+ * @author     : <thiagolimasp@live.com> Thiago Lima
+ * @description: Instances of object Server { aap, port }
  */
 
 import * as express from "express";
@@ -14,8 +11,8 @@ import * as bodyParser from 'body-parser';
 
 export class Server {
   /**
-   * @param app - { express application }
-   * @param port - { port to listen on }
+   * @param: app - { express application }
+   * @param: port - { port to listen on }
    */
   constructor(private app: express.Express, private port: number) {
     this.configureMiddleware(app);
@@ -27,17 +24,17 @@ export class Server {
   }
 
   private configureRoutes(app: express.Express) {
-    app.get("/user", userRouter);
-    app.get("/users", userRouter);
-    app.get("/user/:id", userRouter);
+    app.use('/api', userRouter);
+    app.get('/users', userRouter);
+    app.get('/user/:id', userRouter);
     app.put('/user/:id', userRouter);
-    app.delete("/user/:id", userRouter);
-    app.post("/signup", userRouter);
+    app.delete('/user/:id', userRouter);
+    app.post('/signup', userRouter);
     app.post("/authenticate", userRouter);
   }
 
   /**
-   * @param run the app - express application
+   * @param: run the app - express application
    */
 
   public run() {
