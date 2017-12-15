8cda9b2d0b4369146f7f613203b97802c00bdabb
Thiago Lima <thitheguy@gmail.com>
2017-12-15 03:42:57 -0200

    Creating User Directive

---
+++ public/js/directives/userSection.js
@@ -0,0 +1,14 @@
+/**
+ * @name: headSection.js
+ * @description: table set component table-section
+ * Binding data to its scope object.
+ */
+
+app.directive("userSection", function(){
+    return{
+      restrict: "EA",
+      templateUrl: 'js/directives/views/userSection.html',
+      controllerAs:'vm',
+      bindToController: true
+    };
+ });
