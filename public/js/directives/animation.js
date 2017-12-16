313aaf4ff8446da61c988e1600274f54a91c5cb7
Thiago Lima <thitheguy@gmail.com>
2017-12-16 05:47:36 -0200

    Code review

--- public/js/directives/animation.js
+++ public/js/directives/animation.js
@@ -1,5 +1,5 @@
 /**
- * @name: animate.js
+ * @name       : animate.js
  * @description: Main file to write the Java Script AngularJS animations.
  * Basicaly animations won't require this file because most of them are
  * based on ng-show/hide tags, something special may be written here.
