f860a1701efc5695c331e432fdaa2b7fa6ef9949
Thiago Lima <thitheguy@gmail.com>
2017-12-17 01:09:27 -0200

    Form validation

--- public/js/controller/userController.js
+++ public/js/controller/userController.js
@@ -4,11 +4,15 @@
  * @description: User section objects are set here.
  */
 
- // Load up controller
- angular.module('app').controller("UserController", UserController);
+// Load up controller
+angular.module("app").controller("UserController", UserController);
+angular
+  .module("app")
+  .controller("UserMessagesController", UserMessagesController);
 
 // dependency injection
 UserController.$inject = ["UserService", "$scope"];
+UserMessagesController.$inject = ["$scope"];
 
 function UserController(UserService) {
   var vm = this;
@@ -26,5 +30,24 @@ function UserController(UserService) {
 
   vm.edit = function(id) {
     vm.newuser = angular.copy(UserService.get(id));
+  };
+
+  vm.regex = {
+    name: "^[a-zA-Z]*$",
+    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    phone: "/^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-s./0-9]*$/g"
+  };
+}
+
+function UserMessagesController() {
+  var vm = this;
+
+  vm.messages = {
+    settings: [
+      { name: "required", message: "Can't leave this field empty" },
+      { name: "pattern", message: "Wrong pattern" },
+      { name: "minlength", message: "Too short!" },
+      { name: "maxlength", message: "Too long!" }
+    ]
   };
 }
