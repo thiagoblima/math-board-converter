5066e0594237c39a7ac83294587dbe805b0f2139
Thiago Lima <thitheguy@gmail.com>
2017-12-17 20:56:56 -0200

    Contact front-end refactor

---
+++ public/js/services/ContactService.js
@@ -0,0 +1,59 @@
+/**
+ * @name       : ContactService
+ * @author     : <thiagolimasp@live.com> Thiago Lima
+ * @description: Contact services are set here.
+ */
+
+angular.module("app").service("ContactService", ContactService);
+
+function ContactService() {
+  var uid = 1;
+
+  var contacts = [
+    {
+      id: 0,
+      name: "test",
+      email: "test@email.com",
+      phone: 123234344
+    }
+  ];
+
+  this.save = function(contact) {
+    if (!contact) {
+      console.log('User is not defined!');
+    }else{
+      if (contact.id == null) {
+        contact.id = uid++;
+        contacts.push(contact);
+      } else {
+        //for existing contact, find this contact using id
+        //and update it.
+        for (i in contacts) {
+          if (contacts[i].id == contact.id) {
+            contacts[i] = contact;
+          }
+        }
+      }
+    }
+  };
+
+  this.get = function(id) {
+    for (i in contacts) {
+      if (contacts[i].id == id) {
+        return contacts[i];
+      }
+    }
+  };
+
+  this.delete = function(id) {
+    for (i in contacts) {
+      if (contacts[i].id == id) {
+        contacts.splice(i, 1);
+      }
+    }
+  };
+
+  this.list = function() {
+    return contacts;
+  };
+}
