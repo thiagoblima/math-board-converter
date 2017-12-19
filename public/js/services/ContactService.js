4592ca18b3b40a69f25bf3d0450c9f0f66cb60c4
Thiago Lima <thitheguy@gmail.com>
2017-12-19 05:20:14 -0200

    Adding strict mode

--- public/js/services/ContactService.js
+++ public/js/services/ContactService.js
@@ -4,56 +4,59 @@
  * @description: Contact services are set here.
  */
 
-angular.module("app").service("contactService", contactService);
+(function() {
+  'use strict';
+  angular.module("app").service("contactService", contactService);
 
-function contactService() {
-  var uid = 1;
+  function contactService() {
+    var uid = 1;
 
-  var contacts = [
-    {
-      id: 0,
-      name: "testuser",
-      email: "test@email.com",
-      phone: 123234344
-    }
-  ];
+    var contacts = [
+      {
+        id: 0,
+        name: "testuser",
+        email: "test@email.com",
+        phone: 123234344
+      }
+    ];
 
-  this.save = function(contact) {
-    if (!contact) {
-      console.log('User is not defined!');
-    }else{
-      if (contact.id == null) {
-        contact.id = uid++;
-        contacts.push(contact);
+    this.save = function(contact) {
+      if (!contact) {
+        console.log("User is not defined!");
       } else {
-        //for existing contact, find this contact using id
-        //and update it.
-        for (i in contacts) {
-          if (contacts[i].id == contact.id) {
-            contacts[i] = contact;
+        if (contact.id == null) {
+          contact.id = uid++;
+          contacts.push(contact);
+        } else {
+          //for existing contact, find this contact using id
+          //and update it.
+          for (i in contacts) {
+            if (contacts[i].id == contact.id) {
+              contacts[i] = contact;
+            }
           }
         }
       }
-    }
-  };
+    };
 
-  this.get = function(id) {
-    for (i in contacts) {
-      if (contacts[i].id == id) {
-        return contacts[i];
+    this.get = function(id) {
+      for (i in contacts) {
+        if (contacts[i].id == id) {
+          return contacts[i];
+        }
       }
-    }
-  };
+    };
 
-  this.delete = function(id) {
-    for (i in contacts) {
-      if (contacts[i].id == id) {
-        contacts.splice(i, 1);
+    this.delete = function(id) {
+      for (i in contacts) {
+        if (contacts[i].id == id) {
+          contacts.splice(i, 1);
+        }
       }
-    }
-  };
+    };
 
-  this.list = function() {
-    return contacts;
-  };
-}
+    this.list = function() {
+      return contacts;
+    };
+  }
+})();
