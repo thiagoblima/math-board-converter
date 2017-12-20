3ade6ae8d4d73f56c15484aeafd0b59e99cdf1e5
Thiago Lima <thitheguy@gmail.com>
2017-12-20 04:46:55 -0200

    Setting the file upload as optional for saving an user

--- server/models/user.ts
+++ server/models/user.ts
@@ -11,74 +11,76 @@ const Schema = mongoose.Schema;
 
 // user model settings
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        required: 'Email address is required',
-        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-    },
-    firstname: {
-        type: String,
-        required: true,
-
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    file: {
-        type: String,
-        required: true
-    }, 
-    path: {
-        type: String,
-        required: true,
-        trim: true
-    },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address"
+    ]
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  file: {
+    type: String,
+    required: false
+  },
+  path: {
+    type: String,
+    required: false,
+    trim: true
+  }
 });
 
-UserSchema.pre('save', function (next) {
-    let user = this;
-    if (this.isModified('password') || this.isNew) {
-        bcrypt.genSalt(10, function (err, salt) {
-            if (err) {
-                return next(err);
-            }
-            bcrypt.hash(user.password, salt, function (err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
-});
-
-UserSchema.methods.comparePassword = function (passw, cb) {
-    bcrypt.compare(passw, this.password, function (err, isMatch) {
+UserSchema.pre("save", function(next) {
+  let user = this;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function(err, hash) {
         if (err) {
-            return cb(err);
+          return next(err);
         }
-        cb(null, isMatch);
+        user.password = hash;
+        next();
+      });
     });
+  } else {
+    return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
 };
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model("User", UserSchema);
