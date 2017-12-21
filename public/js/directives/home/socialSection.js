b9d38309cd33f4be65c46b7d0da3de86631d53b4
Thiago Lima <thitheguy@gmail.com>
2017-12-21 02:59:36 -0200

    New home component /directive structure

---
+++ public/js/directives/home/socialSection.js
@@ -0,0 +1,13 @@
+/**
+ * @name       : socialSection.js
+ * @description: social set component social-section
+ * Binding data to its scope object.
+ */
+
+angular.module('app').directive("socialSection", function() {
+  return {
+    restrict: "E",
+    scope: {},
+    templateUrl: "js/directives/home/views/socialSection.html"
+  };
+});
