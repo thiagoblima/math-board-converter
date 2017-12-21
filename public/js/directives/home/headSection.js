032c5914b983f15a6fc35edf771076c75dd6b6e1
Thiago Lima <thitheguy@gmail.com>
2017-12-21 02:58:33 -0200

    New home component /directive structure

---
+++ public/js/directives/home/headSection.js
@@ -0,0 +1,23 @@
+/**
+ * @name       : headSection.js
+ * @description: head set component head-section
+ * Binding data to its scope object.
+ */
+
+angular.module('app').directive("headSection", function() {
+  return {
+    restrict: "EA",
+    scope: {
+      data: "="
+    },
+    templateUrl: "js/directives/home/views/headSection.html",
+    controllerAs: "vm",
+    link: function(data, element, attrs) {
+      data.showMe = false;
+      data.searchOpen = function() {
+        data.showMe = !data.showMe;
+      };
+      data.search = "Search a Math Info...";
+    }
+  };
+});
