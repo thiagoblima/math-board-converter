4bade883128be628ef43b9f843b57861e8dc169f
Thiago Lima <thitheguy@gmail.com>
2017-12-21 02:57:19 -0200

    New home component /directive structure

---
+++ public/js/directives/home/contactMessages.js
@@ -0,0 +1,15 @@
+/**
+ * @name       : ContactMessagesSection.js
+ * @description: messages component contact-messages
+ * Binding data to its scope object.
+ */
+
+
+angular.module('app').directive('contactMessages', function(){
+    return{
+       restrict:"AE",
+       templateUrl:'js/directives/home/views/contactMessages.html',
+       controllerAs:"vm",
+       bindToController: true
+    };
+ });
