da37047780cdc625bb328e9511a651ebfa53751d
Thiago Lima <thitheguy@gmail.com>
2017-12-21 00:13:14 -0200

    Lots of changes. 1 ) - Cleaning task process 2 ) - View folder building config 3 ) - Node_modules folder instead of libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,31 @@
 /**
- * @name: Gruntfile.js
- * @author: <thiagolimasp@live.com> Thiago Lima
- * @description: Responsible for the building process upon Java Script, 
- * SASS, CSS, cleaning folders and time stamp it after the building 
- * process, this way it's easier to always let the server know the date 
+ * @name       : Gruntfile.js
+ * @author     : <thiagolimasp@live.com> Thiago Lima
+ * @description: Responsible for the building process upon Java Script,
+ * SASS, CSS, cleaning folders and time stamp it after the building
+ * process, this way it's easier to always let the server know the date
  * of the building process was taken to (cache-buster principles).
  */
 
-'use strict';
+"use strict";
 
-module.exports = (grunt) => {
-
-  const cacheHash = ((new Date()).valueOf().toString()) + (Math.floor((Math.random() * 1000000) + 1).toString());
+module.exports = grunt => {
+  const cacheHash =
+    new Date().valueOf().toString() +
+    Math.floor(Math.random() * 1000000 + 1).toString();
   console.log("This cached version: ", cacheHash);
 
   // initializing load-grunt-tasks
 
   function loader() {
-    return 'load-grunt-tasks';
-  };
+    return "load-grunt-tasks";
+  }
 
   require(loader())(grunt);
 
   grunt.initConfig({
-
-    pkg: grunt.file.readJSON('package.json'),
-    deploy: grunt.file.readJSON('deploy.json'),
+    pkg: grunt.file.readJSON("package.json"),
+    deploy: grunt.file.readJSON("deploy.json"),
 
     /**
      * @description: Here some properties will store paths for building process
@@ -35,7 +35,7 @@ module.exports = (grunt) => {
     srcpath: "public",
     directivespath: "build/js/directives/views",
     directivesjs: "build/js/app",
-    webfontpath: "<%= srcpath %>/libs/bootstrap/fonts",
+    webfontpath: "<%= srcpath %>/node_modules/bootstrap/fonts",
     mathpath: "<%= srcpath %>/build/js/math",
     path: "build",
     cssbuild: "css",
@@ -43,163 +43,149 @@ module.exports = (grunt) => {
     directivesbuild: "<%= jsbuild %>/app",
     webfontbuild: "<%= path %>/fonts",
 
-
     jsfiles: [
-
       /**
        * @description: Here goes the author Angular.js scripts
-       *
-       * @see: All of them are based on the root: /public/js/
-       * Hopefully any change for the next updates
-      */
+       * @see        : All of them are based on the root:
+       * /public/js/ Hopefully any change for the next updates
+       */
 
-      '<%= vendorjs %>',
-      '<%= srcpath %>/js/app.js',
-      '<%= srcpath %>/js/controller/MathController.js',
-      '<%= srcpath %>/js/controller/TableController.js',
-      '<%= srcpath %>/js/controller/InfoController.js',
-      '<%= srcpath %>/js/controller/ContactController.js',
-      '<%= srcpath %>/js/controller/UserController.js',
-      '<%= mathjsfiles %>'
+      "<%= vendorjs %>",
+      "<%= srcpath %>/js/app.js",
+      "<%= srcpath %>/js/controller/MathController.js",
+      "<%= srcpath %>/js/controller/TableController.js",
+      "<%= srcpath %>/js/controller/InfoController.js",
+      "<%= srcpath %>/js/controller/ContactController.js",
+      "<%= srcpath %>/js/controller/UserController.js",
+      "<%= mathjsfiles %>"
     ],
 
     vendorjs: [
-
       /**
        * @description: Here goes the vendor's scripts
-       *
-       * @see: All of them are based on the root: /public/libs/
-       * Hopefully any change for the next updates
-      */
+       * @see        : All of them are based on the
+       * root: /public/node_modules/ Hopefully any change
+       * for the next updates.
+       */
 
-      '<%= srcpath %>/node_modules/angular/angular.min.js',
-      '<%= srcpath %>/node_modules/angular-sanitize/angular-sanitize.min.js',
-      '<%= srcpath %>/node_modules/angular-animate/angular-animate.min.js',
-      '<%= srcpath %>/node_modules/angular-messages/angular-messages.min.js',
-      '<%= srcpath %>/node_modules/angular-ui-mask/dist/mask.min.js',
-      '<%= srcpath %>/node_modules/jquery/dist/jquery.min.js',
-      '<%= srcpath %>/node_modules/bootstrap/dist/js/bootstrap.min.js',
+      "<%= srcpath %>/node_modules/angular/angular.min.js",
+      "<%= srcpath %>/node_modules/angular-route/angular-route.min.js",
+      "<%= srcpath %>/node_modules/angular-sanitize/angular-sanitize.min.js",
+      "<%= srcpath %>/node_modules/angular-animate/angular-animate.min.js",
+      "<%= srcpath %>/node_modules/angular-messages/angular-messages.min.js",
+      "<%= srcpath %>/node_modules/angular-ui-mask/dist/mask.min.js",
+      "<%= srcpath %>/node_modules/jquery/dist/jquery.min.js",
+      "<%= srcpath %>/node_modules/bootstrap/dist/js/bootstrap.min.js"
     ],
 
     angularjsfiles: [
-
       /**
        * @description: Here goes the AngularJS Directives
-       *
-       * @see: They're basicly distributed on public/js/directives/
+       * @see        : They're basicly distributed on public/js/directives/
        * Hopefully any change for the next updates
-      */
+       */
 
-      '<%= srcpath %>/js/directives/headSection.js',
-      '<%= srcpath %>/js/directives/introSection.js',
-      '<%= srcpath %>/js/directives/mainNav.js',
-      '<%= srcpath %>/js/directives/socialsection.js',
-      '<%= srcpath %>/js/directives/scroll.js',
-      '<%= srcpath %>/js/directives/guideSection.js',
-      '<%= srcpath %>/js/directives/tableSection.js',
-      '<%= srcpath %>/js/directives/tableMessages.js',
-      '<%= srcpath %>/js/directives/userSection.js',
-      '<%= srcpath %>/js/directives/userMessages.js',
-      '<%= srcpath %>/js/directives/contactSection.js',
-      '<%= srcpath %>/js/directives/contactMessages.js',
-      '<%= srcpath %>/js/services/ContactService.js',
-      '<%= srcpath %>/js/services/UserService.js',
-      '<%= srcpath %>/js/filters/searchFilter.js',
-      '<%= srcpath %>/js/filters/mathFilter.js',
-      '<%= srcpath %>/js/filters/animation.js'
-
+      "<%= srcpath %>/js/directives/headSection.js",
+      "<%= srcpath %>/js/directives/introSection.js",
+      "<%= srcpath %>/js/directives/mainNav.js",
+      "<%= srcpath %>/js/directives/socialsection.js",
+      "<%= srcpath %>/js/directives/scroll.js",
+      "<%= srcpath %>/js/directives/guideSection.js",
+      "<%= srcpath %>/js/directives/tableSection.js",
+      "<%= srcpath %>/js/directives/tableMessages.js",
+      "<%= srcpath %>/js/directives/userSection.js",
+      "<%= srcpath %>/js/directives/userMessages.js",
+      "<%= srcpath %>/js/directives/contactSection.js",
+      "<%= srcpath %>/js/directives/contactMessages.js",
+      "<%= srcpath %>/js/services/ContactService.js",
+      "<%= srcpath %>/js/services/UserService.js",
+      "<%= srcpath %>/js/filters/searchFilter.js",
+      "<%= srcpath %>/js/filters/mathFilter.js",
+      "<%= srcpath %>/js/filters/animation.js"
     ],
 
     mathjsfiles: [
-
       /**
        * @description: Here goes the Math app Java Script
-       *
-       * @see: They're basicly distributed on public/js/math/
+       * @see        : They're basicly distributed on public/js/math/
        * Hopefully any change for the next updates
        */
-
       /*'<%= srcpath %>/js/math/model/number.js',
       '<%= srcpath %>/js/math/view/number_view.js'*/
-
     ],
 
     htmlpages: [
-
       /**
        * @description: Here goes the html pages
-       *
-       * @see: All of them are based on the root: /public/
+       * @see         : All of them are based on the root: /public/
        * Hopefully any change for the next updates
        */
 
-      '<%= srcpath %>/index.html'
+      "<%= srcpath %>/index.html"
     ],
-
+    htmlviews: ["<%= srcpath %>/views/welcome.html"],
     directivespages: [
-
       /**
        * @description: Here goes the AngularJS Directives (HTML)
-       *
-       * @see: They're basicly distributed on public/js/directives/
-       * Hopefully any change for the next updates
+       * @see        : They're basicly distributed on
+       * public/js/directives/ Hopefully any change for
+       * the next updates.
        */
 
-      '<%= srcpath %>/js/directives/views/headSection.html',
-      '<%= srcpath %>/js/directives/views/introSection.html',
-      '<%= srcpath %>/js/directives/views/socialSection.html',
-      '<%= srcpath %>/js/directives/views/mainNav.html',
-      '<%= srcpath %>/js/directives/views/guideSection.html',
-      '<%= srcpath %>/js/directives/views/tableSection.html',
-      '<%= srcpath %>/js/directives/views/tableMessages.html',
-      '<%= srcpath %>/js/directives/views/contactSection.html',
-      '<%= srcpath %>/js/directives/views/userSection.html',
-      '<%= srcpath %>/js/directives/views/contactMessages.html',
-      '<%= srcpath %>/js/directives/views/userMessages.html'
-      
-
-
+      "<%= srcpath %>/js/directives/views/headSection.html",
+      "<%= srcpath %>/js/directives/views/introSection.html",
+      "<%= srcpath %>/js/directives/views/socialSection.html",
+      "<%= srcpath %>/js/directives/views/mainNav.html",
+      "<%= srcpath %>/js/directives/views/guideSection.html",
+      "<%= srcpath %>/js/directives/views/tableSection.html",
+      "<%= srcpath %>/js/directives/views/tableMessages.html",
+      "<%= srcpath %>/js/directives/views/contactSection.html",
+      "<%= srcpath %>/js/directives/views/userSection.html",
+      "<%= srcpath %>/js/directives/views/contactMessages.html",
+      "<%= srcpath %>/js/directives/views/userMessages.html"
     ],
 
     webfonts: [
-
       /**
        * @description: Here goes the WebFonts and Icons (bootstrap)
-       *
-       * @see: They're basicly distributed on public/libs/bootstrap/fonts
+       * @see        : They're basicly distributed on public/node_modules/bootstrap/fonts
        * Hopefully any change for the next updates
        */
 
-      '<%= webfontpath %>/*'
-
+      "<%= webfontpath %>/*"
     ],
 
     clean: {
       build: {
         src: [
-          '<%= path %>/js/*.js', 
-          '<%= path %>/js/app/*.js', 
-          'routes',
-          'models',
-          'config',
-          'middleware', 
-          'server.js',
-          'app.js'
+          "<%= path %>/js/*.js",
+          "<%= path %>/js/app/*.js",
+          ".sass-cache",
+          "build",
+          "routes",
+          "models",
+          "config",
+          "middleware",
+          "server.js",
+          "app.js"
         ]
       }
     },
 
     htmlbuild: {
       html: {
-        src: '<%= htmlpages %>',
-        dest: '<%= path %>'
+        src: "<%= htmlpages %>",
+        dest: "<%= path %>"
       },
       angularjs: {
-        src: '<%= directivespages %>',
-        dest: '<%= directivespath %>'
+        src: "<%= directivespages %>",
+        dest: "<%= directivespath %>"
+      },
+      views: {
+        src: "<%= htmlviews %>",
+        dest: "<%= path %>/views/"
       }
     },
-
 
     copy: {
       /*angularjs: {
@@ -212,51 +198,54 @@ module.exports = (grunt) => {
       webfonts: {
         expand: true,
         flatten: true,
-        src: '<%= webfonts %>',
-        dest: '<%= webfontbuild %>'
+        src: "<%= webfonts %>",
+        dest: "<%= webfontbuild %>"
       }
     },
 
     eslint: {
-      target: ['public/js/**/*.js'],
+      target: ["public/js/**/*.js"],
       options: {
-        config: '.eslintrc.json'
+        config: ".eslintrc.json"
       }
     },
 
     uglify: {
       options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n',
+        banner:
+          '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n',
         compress: false,
         beautify: false,
         mangle: false
       },
       build: {
         files: {
-          '<%= path %>/js/devbuild.<%= hash %>.min.js': ["<%= jsfiles %>"],
-          '<%= directivesjs %>/angularjs.<%= hash %>.min.js': ["<%= angularjsfiles %>"]
+          "<%= path %>/js/devbuild.<%= hash %>.min.js": ["<%= jsfiles %>"],
+          "<%= directivesjs %>/angularjs.<%= hash %>.min.js": [
+            "<%= angularjsfiles %>"
+          ]
         }
       }
     },
-
 
     sass: {
       build: {
         files: {
-          '<%= path %>/css/main.css': '<%= srcpath %>/themes/sass/screen.scss'
+          "<%= path %>/css/main.css": "<%= srcpath %>/themes/sass/screen.scss"
         }
       }
     },
 
-
     cssmin: {
       options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+        banner:
+          '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
       },
       build: {
         files: {
           //'<%= path %>/css/screen.min.css': '<%= srcpath %>/themes/stylesheets/screen.css',
-          '<%= path %>/css/vendor.min.css': '<%= srcpath %>/libs/bootstrap/dist/css/bootstrap.min.css'
+          "<%= path %>/css/vendor.min.css":
+            "<%= srcpath %>/node_modules/bootstrap/dist/css/bootstrap.min.css"
         }
       }
     },
@@ -267,65 +256,100 @@ module.exports = (grunt) => {
           "./": ".json"
         },
         options: {
-          path: '/tmp/',
-          host: '<%= deploy.host %>',
-          username: '<%= deploy.username %>',
-          password: '<%= deploy.password %>',
+          path: "/tmp/",
+          host: "<%= deploy.host %>",
+          username: "<%= deploy.username %>",
+          password: "<%= deploy.password %>",
           showProgress: true
         }
       }
     },
     sshexec: {
       test: {
-        command: 'uptime',
+        command: "uptime",
         options: {
-          host: '<%= deploy.host %>',
-          username: '<%= deploy.username %>',
-          password: '<%= deploy.password %>'
+          host: "<%= deploy.host %>",
+          username: "<%= deploy.username %>",
+          password: "<%= deploy.password %>"
         }
       }
     },
 
     watch: {
       stylesheets: {
-        files: ['<%= srcpath %>/themes/stylesheets/**/*.css', '<%= srcpath %>/themes/sass/**/*.scss'],
-        tasks: ['cssmin', 'sass'],
+        files: [
+          "<%= srcpath %>/themes/stylesheets/**/*.css",
+          "<%= srcpath %>/themes/sass/**/*.scss"
+        ],
+        tasks: ["cssmin", "sass"],
         options: {
           livereload: true
         }
       },
       html: {
-        files: ['<%= htmlpages %>', '<%= directivespages %>'],
-        tasks: ['uglify', 'htmlbuild'],
+        files: ["<%= htmlpages %>", "<%= directivespages %>"],
+        tasks: ["uglify", "htmlbuild"],
         opstions: {
           livereload: true
         }
       },
       scripts: {
-        files: '<%= srcpath %>/js/**/*.js',
-        tasks: ['eslint', 'uglify', 'htmlbuild'],
+        files: "<%= srcpath %>/js/**/*.js",
+        tasks: ["eslint", "uglify", "htmlbuild"],
         options: {
           livereload: true
         }
       }
     }
-
   });
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-html-build');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-ssh');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-html-build");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-ssh");
 
-  grunt.registerTask('default', ['eslint']);
-  grunt.registerTask('dev', ['clean', 'eslint', 'htmlbuild', 'copy', 'uglify', 'cssmin', 'sass']);
-  grunt.registerTask('cleanup', ['clean','copy', 'eslint']);
-  grunt.registerTask('production', ['clean', 'eslint', 'htmlbuild', 'copy', 'uglify', 'cssmin', 'sass']);
-  grunt.registerTask('dev-deploy', ['clean', 'eslint', 'htmlbuild', 'copy', 'uglify', 'cssmin', 'sass', 'sshexec']);
-  grunt.registerTask('production-deploy', ['clean', 'eslint', 'htmlbuild', 'copy', 'uglify', 'cssmin', 'sass', 'sshexec']);
-
+  grunt.registerTask("default", ["eslint"]);
+  grunt.registerTask("dev", [
+    "clean",
+    "eslint",
+    "htmlbuild",
+    "copy",
+    "uglify",
+    "cssmin",
+    "sass"
+  ]);
+  grunt.registerTask("cleanup", ["clean", "copy", "eslint"]);
+  grunt.registerTask("production", [
+    "clean",
+    "eslint",
+    "htmlbuild",
+    "copy",
+    "uglify",
+    "cssmin",
+    "sass"
+  ]);
+  grunt.registerTask("dev-deploy", [
+    "clean",
+    "eslint",
+    "htmlbuild",
+    "copy",
+    "uglify",
+    "cssmin",
+    "sass",
+    "sshexec"
+  ]);
+  grunt.registerTask("production-deploy", [
+    "clean",
+    "eslint",
+    "htmlbuild",
+    "copy",
+    "uglify",
+    "cssmin",
+    "sass",
+    "sshexec"
+  ]);
 };
