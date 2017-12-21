54d4acb2ef50f95b588d370ab957aae4eaec0a28
Thiago Lima <thitheguy@gmail.com>
2017-12-21 02:58:16 -0200

    New home component /directive structure

---
+++ public/js/directives/home/guideSection.js
@@ -0,0 +1,14 @@
+/**
+ * @name       : guideSection.js
+ * @description: directive component guide-section 
+ * Binding data to its scope object.
+ */
+
+angular.module('app').directive("guideSection", function() {
+  return {
+    restrict: "EA",
+    templateUrl: "js/directives/home/views/guideSection.html",
+    controllerAs: "vm",
+    bindToController: true
+  };
+});
