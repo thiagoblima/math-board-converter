75779cbd7f25ccbb57dbb098769d55cda126bdf8
Thiago Lima <thitheguy@gmail.com>
2017-12-26 18:22:03 -0200

    Adding head-animation (HeadController)

--- public/js/directives/includes/headSection.js
+++ public/js/directives/includes/headSection.js
@@ -7,17 +7,8 @@
 angular.module('app').directive("headSection", function() {
   return {
     restrict: "EA",
-    scope: {
-      data: "="
-    },
     templateUrl: "js/directives/includes/views/headSection.html",
-    controllerAs: "vm",
-    link: function(data, element, attrs) {
-      data.showMe = false;
-      data.searchOpen = function() {
-        data.showMe = !data.showMe;
-      };
-      data.search = "Search a Math Info...";
-    }
+    controller: "HeadController",
+    controllerAs: "vm"
   };
 });
