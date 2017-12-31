b6c2cce9ed99fb197492c850ddbd9b4bdb56ef38
Thiago Lima <thitheguy@gmail.com>
2017-12-31 01:22:21 -0200

    ES6 Front-end Migration

--- public/js/directives/home/userSection.js
+++ public/js/directives/home/userSection.js
@@ -3,12 +3,16 @@
  * @description: user set component <user-section>
  * Binding data to its scope object.
  */
-
-angular.module("app").directive("userSection", function() {
-    return {
+export default class userSection {
+  constructor() {
+    this.directive = {
       restrict: "EA",
       templateUrl: "js/directives/home/views/userSection.html",
       controllerAs: "vm",
       bindToController: true
     };
-  });
+  }
+  ngInit() {
+    return this.directive;
+  }
+}
