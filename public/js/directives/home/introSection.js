3587a30d82cb29135e7d716807ccf660e69058be
Thiago Lima <thitheguy@gmail.com>
2017-12-31 01:24:28 -0200

    ES6 Front-end Migration

--- public/js/directives/home/introSection.js
+++ public/js/directives/home/introSection.js
@@ -1,14 +1,20 @@
 /**
  * @name       : introSection.js
- * @description: intro set component intro-section
+ * @description: intro section component <intro-section>
  * Binding data to its scope object.
  */
 
-angular.module('app').directive("introSection", function() {
-  return {
-    restrict: "EA",
-    templateUrl: "js/directives/home/views/introSection.html",
-    controllerAs: "vm",
-    bindToController: true
-  };
-});
+export default class introSection {
+  constructor() {
+    this.directive = {
+      restrict: "EA",
+      templateUrl: "js/directives/home/views/introSection.html",
+      controllerAs: "vm",
+      bindToController: true
+    };
+  }
+
+  ngInit() {
+    return this.directive;
+  }
+}
