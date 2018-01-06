751f63eafc34b3b64c426386b6f7f115d4319f42
Thiago Lima <thitheguy@gmail.com>
2018-01-06 00:29:54 -0200

    ES6 for user controller

--- public/js/controller/userController.js
+++ public/js/controller/userController.js
@@ -4,45 +4,43 @@
  * @description: User section objects are set here.
  */
 
-(function() {
-  "use strict";
-  // Load up controller
-  angular.module("app").controller("UserController", UserController);
-  angular
-    .module("app")
-    .controller("UserMessagesController", UserMessagesController);
+import userServices from "../services/userService";
 
-  // dependency injection
-  UserController.$inject = ["userServices", "$scope"];
-
-  function UserController(userServices) {
-    var vm = this;
-    vm.user = {};
-    vm.user.path = "../../assets/";
-
-    vm.addUser = function(user) {
-      userServices
-        .postUser(user)
-        .then(function(data) {
-          console.log("User successfully saved!", data);
-        })
-        .catch("Error while saving the user!");
-    };
-
-    vm.regex = {
-      username: "^[a-zA-Z0-9]*$",
-      password: "^[a-zA-Z0-9]*$",
-      email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      firstname: "^[a-zA-Z]*$",
-      lastname: "^[a-zA-Z]*$",
-      age: "^[0-9]*$"
-    };
+export class UserController {
+  constructor(userServices) {
+    this.userServices = userServices;
+    this.user = {};
+    this.regex = this.regex();
   }
 
-  function UserMessagesController() {
-    var vm = this;
+  addUser(user) {
+    this.userServices
+      .postUser(this.user)
+      .then(data => {
+        console.log("User successfully saved!", data);
+      })
+      .catch("Error while saving the user!");
+  }
 
-    vm.messages = {
+  regex() {
+    return {
+      username: /^[a-zA-Z0-9]*$/,
+      password: /^[a-zA-Z0-9]*$/,
+      email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      firstname: /^[a-zA-Z]*$/,
+      lastname: /^[a-zA-Z]*$/,
+      age: /^[0-9]*$/
+    };
+  }
+}
+
+export class UserMessagesController {
+  constructor() {
+    this.messages = this.messages();
+  }
+
+  messages() {
+    return {
       settings: [
         { name: "required", message: "Can't leave this field empty" },
         { name: "pattern", message: "Wrong pattern" },
@@ -51,4 +49,9 @@
       ]
     };
   }
-})();
+}
+// Load up controller
+angular.module("app").controller("UserController", UserController);
+angular
+  .module("app")
+  .controller("UserMessagesController", UserMessagesController);
