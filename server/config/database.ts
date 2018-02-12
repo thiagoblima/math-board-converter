422bc45df2a78532fc9c649623a0eb2880920228
Thiago Lima <thitheguy@gmail.com>
2018-02-12 02:59:23 -0200

    Removing unnecessary files

--- server/config/database.ts
+++ server/config/database.ts
@@ -11,7 +11,7 @@ export class MongoDB {
   private db: string;
   private secret: string;
   constructor() {
-    this.db = "mongodb://'heroku_xz9ldbph':'mathdb01'@ds233228.mlab.com:33228/heroku_xz9ldbph";
+    this.db = "mongodb://localhost/math";
     this.secret = "myjwtsecretkey";
   }
 
